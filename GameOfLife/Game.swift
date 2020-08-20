1be2c9828c13ce32b76ed0811e47d8bf1b8abb15
John Kouris <jdkouris@gmail.com>
2020-08-19 18:20:02 -0700

    Started implementing the game logic

---
+++ GameOfLife/Game.swift
@@ -0,0 +1,65 @@
+//
+//  Game.swift
+//  GameOfLife
+//
+//  Created by John Kouris on 8/18/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    let width: Int
+    let height: Int
+    var currentState: GameState
+    
+    init(width: Int, height: Int) {
+        self.width = width
+        self.height = height
+        let cells = Array(repeating: Cell.makeDeadCell(), count: width * height)
+        currentState = GameState(cells: cells)
+    }
+    
+    func iterate() -> GameState {
+        var nextState = currentState
+        for i in 0...width - 1 {
+            for j in 0...height - 1 {
+                let positionInTheArray = j * width + i
+                nextState[positionInTheArray] = Cell(isAlive: state(x: i, y: j))
+            }
+        }
+        
+        self.currentState = nextState
+        return nextState
+    }
+    
+    func state(x: Int, y: Int) -> Bool {
+        let numberOfAliveNeighbours = aliveNeighborCountAt(x: x, y: y)
+        let position = x + y*width
+        
+        let wasPreviouslyAlive = currentState[position].isAlive
+        if wasPreviouslyAlive {
+            return numberOfAliveNeighbours == 2 || numberOfAliveNeighbours == 3
+        } else {
+            return numberOfAliveNeighbours == 3
+        }
+    }
+    
+    func aliveNeighborCountAt(x: Int, y: Int) -> Int {
+        var numberOfAliveNeighbours = 0
+        for i in x-1...x+1 {
+            for j in y-1...y+1 {
+                if (i == x && y == j) || (i >= width) || (i < 0) || (j < 0) { continue }
+                
+                let index = j*width + i
+                
+                guard index >= 0 && index < width*height else { continue }
+                if currentState[index].isAlive {
+                    numberOfAliveNeighbours += 1
+                }
+            }
+        }
+        return numberOfAliveNeighbours
+    }
+    
+}
